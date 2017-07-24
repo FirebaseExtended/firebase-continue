45149e8bfa694d5bf5e3326dae891b875ef0dbb8
Andreas Ricci <riccia@google.com>
2017-07-24 16:52:54 -0400

    (Web) Add My Notes page with list of notes and option to add, delete, or open to edit a note

--- samples/web/Continote/public/scripts/utils.js
+++ samples/web/Continote/public/scripts/utils.js
@@ -20,11 +20,6 @@
  * Remember to include this script in the <head> of a page if you plan on
  * using any of these utilities within that page.
  *
- * TODO: Consider setting up a "common" folder for the Chrome extension sample
- * and the web app sample to share code, and putting this file there since it
- * currently has plenty of overlap (as of July 21, 2017).
- * If this change is made, remember to update the installation/setup guides!
- *
  * @type {!Object}
  * @const
  */
@@ -40,6 +35,15 @@ var Utils = (function() {
   var hiddenClass_ = "hidden";
 
   /**
+   * This is the class which, if applied to an element, means
+   * the text within the element should be considered a placeholder.
+   *
+   * @type {!string}
+   * @const
+   */
+  var placeholderClass_ = "placeholder";
+
+  /**
    * This is the name of the click event type.
    *
    * @type {!string}
@@ -47,27 +51,56 @@ var Utils = (function() {
    */
   var clickEventType_ = "click";
 
+  /**
+   * Shows the provided DOM element if it is currently hidden.
+   *
+   * @function
+   * @param {!Element} element
+   * @const
+   */
+  var showElement_ = function(element) {
+    element.classList.remove(hiddenClass_);
+  };
+
+  /**
+   * Hides the provided DOM element if it is currently shown.
+   *
+   * @function
+   * @param {!Element} element
+   * @const
+   */
+  var hideElement_ = function(element) {
+    element.classList.add(hiddenClass_);
+  };
+
   return {
+
     /**
-     * Shows the provided DOM element if it is currently hidden.
+     * Shows all DOM elements with the provided className.
      *
      * @function
-     * @param {!Element} element
+     * @param {!string} className
      * @const
      */
-    showElement: function(element) {
-      element.classList.remove(hiddenClass_);
+    showAllElementsWithClassName: function(className) {
+      var elements = document.getElementsByClassName(className);
+      for (var i = 0; i < elements.length; i++) {
+        showElement_(elements[i]);
+      }
     },
 
     /**
-     * Hides the provided DOM element if it is currently shown.
+     * Hides all DOM elements with the provided className.
      *
      * @function
-     * @param {!Element} element
+     * @param {!string} className
      * @const
      */
-    hideElement: function(element) {
-      element.classList.add(hiddenClass_);
+    hideAllElementsWithClassName: function(className) {
+      var elements = document.getElementsByClassName(className);
+      for (var i = 0; i < elements.length; i++) {
+        hideElement_(elements[i]);
+      }
     },
 
     /**
@@ -102,6 +135,68 @@ var Utils = (function() {
       if (clickEventListener) {
         button.removeEventListener(clickEventType_, clickEventListener);
       }
+    },
+
+    /**
+     * Puts the given text, or the given placeholder if the text
+     * is null or empty, within the element.
+     *
+     * @function
+     * @param {?string} text
+     * @param {!string} placeholder
+     * @param {!Element} element
+     * @const
+     */
+    putTextOrPlaceholderInElement: function(text, placeholder, element) {
+      if (text && text.length > 0) {
+        element.textContent = text;
+        element.classList.remove(placeholderClass_);
+      } else {
+        element.textContent = placeholder;
+        element.classList.add(placeholderClass_);
+      }
+    },
+
+    /**
+     * Appends an element based on the provided template to the provided
+     * container.
+     *
+     * @function
+     * @param {!string} template - The template to create the new element
+     * based upon.
+     * @param {!Element} container - The container to append the new element
+     * (created from the template) to.
+     * @returns {?Element} - The new element that is now in the container (or
+     * null if no element could be appended).
+     * @const
+     */
+    appendElementBasedOnTemplateToContainer: function(template, container) {
+      if (!template) {
+        // Invalid template, so do nothing.
+        // This should never happen, but just in case.
+        return null;
+      }
+
+      if (!container) {
+        // Invalid container, so do nothing.
+        // This should never happen, but just in case.
+        return null;
+      }
+
+      // The template will be added to this container
+      // (which will not be anywhere in the actual page DOM) temporarily,
+      // and then moved to the final container.
+      // This is needed so there is an actual element created from the
+      // template.
+      var temporaryContainer = document.createElement("div");
+      temporaryContainer.innerHTML = template;
+
+      // Get the element to put in the actual container from the temporary
+      // container, then append that element to said actual container.
+      var element = temporaryContainer.firstElementChild;
+      container.appendChild(element);
+
+      return element;
     }
   }
 }());
