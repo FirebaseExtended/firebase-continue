51873d4f5652ea27b0b80772ab5b086918416816
Andreas Ricci <riccia@google.com>
2017-07-24 13:17:14 -0400

    WIP: (iOS) Finish My Notes screen of Continote by adding TableView of Notes with option to add, open, or delete notes

--- samples/ios/Continote/Continote/MyNotesViewController.swift
+++ samples/ios/Continote/Continote/MyNotesViewController.swift
@@ -15,12 +15,20 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabaseUI
 
 /**
  This view controller presents the user with a list of their notes in Continote.
  From here the user can open or delete notes.
  */
 class MyNotesViewController: BaseViewController {
+
+  // Firebase Realtime Database reference for the current user's note data within this application.
+  private var notesFirebaseDatabaseRef: DatabaseReference?
+
+  // The FirebaseUI data source to populate the TableView of notes for the current user.
+  private var notesTableViewDataSource: FUITableViewDataSource!
 
   // UI outlets
   @IBOutlet var temporaryTodoLabel: UILabel!
@@ -35,8 +43,55 @@ class MyNotesViewController: BaseViewController {
     temporaryTodoLabel.applyAppTheme(for: .normalText)
   }
 
+  override func handleUserSignedIn(_ user: User) {
+    super.handleUserSignedIn(user)
+
+    // Set up our TableView up to sync with GoogleBroadcasts for the current user.
+    let currentUserId: String! = Auth.auth().user.uid
+    notesFirebaseDatabaseRef = Database.database().reference().child("notes").child(currentUserId)
+    notesTableViewDataSource =
+      notesTableView.bind(to: notesFirebaseDatabaseRef!) { tableView, indexPath, snap in
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell") as! NoteTableViewCell
+        let value:[String : AnyObject] = snap.value as? [String : AnyObject] ?? [:]
+        let url:String = value["url"] as! String
+        let addedAt:Int = value["originPushedAt"] as! Int
+
+        cell.label?.text = "App: \(app)\n" +
+          "Action: \(action)\n" +
+          "URL: \(url)\n" +
+          "Origin Pushed At: \(originPushedAtString)\n" +
+          "Destination Continued At: \(destinationContinuedAtString)"
+
+        // Style the delete button.
+        cell.deleteButton?.setTitleColor(UIColor.red, for: .normal)
+        cell.deleteButton?.backgroundColor = .clear
+        cell.deleteButton?.layer.cornerRadius = 5
+        cell.deleteButton?.layer.borderWidth = 1
+        cell.deleteButton?.layer.borderColor = UIColor.red.cgColor
+
+        // Terrible hack to pass along the key of what should be deleted.
+        cell.deleteButton?.accessibilityHint = snap.key
+        cell.deleteButton?.addTarget(
+          self, action: #selector(deleteButtonPressed), for: .touchUpInside)
+
+        return cell
+      }
+
+    // Now that the TableView is ready, display it.
+    notesTableView.isHidden = false
+  }
+
   override func handleUserSignedOut() {
     super.handleUserSignedOut()
+
+    // Remove all database bindings and observers.
+    notesTableViewDataSource?.unbind()
+    notesTableViewDataSource = nil
+    notesFirebaseDatabaseRef?.removeAllObservers()
+    notesFirebaseDatabaseRef = nil
+
+    // Now that the data source is empty, reload the table to clear out its contents.
+    notesTableView.reloadData()
 
     // The user must be signed in to view this screen, so navigate away from it if the user is
     // signed out.
