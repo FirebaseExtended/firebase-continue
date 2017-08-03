2ce10d9cc4f15c90b9ae8c6d470c1bcdbc707573
David Airapetyan <davidair@google.com>
2017-08-03 14:07:37 -0400

    Introducing a skeleton structure for the iOS library

---
+++ ios/FirebaseContinue/Example/FirebaseContinue/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirebaseContinue
+//
+//  Created by David Airapetyan on 08/01/2017.
+//  Copyright (c) 2017 David Airapetyan. All rights reserved.
+//
+
+@import UIKit;
+#import "FCNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FCNAppDelegate class]));
+    }
+}
